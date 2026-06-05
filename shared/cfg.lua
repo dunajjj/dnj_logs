6a5b096d21e7a29c0c161e042a803c8452feb216
dnj <157141256+dunajjj@users.noreply.github.com>
2026-06-05 10:30:06 +0200

    Update cfg.lua

--- shared/cfg.lua
+++ shared/cfg.lua
@@ -1,11 +1,11 @@
-dnj = {} -- lwk risky davat webhooky do configu xddd mal by som asi updatnut a urobit sv_config ale kto chce nech si to urobi sam vis co 
+dnj = {} -- lwk risky davat webhooky do configu xdd mal by som asi updatnut a urobit sv_config ale kto chce nech si to urobi sam vis co 
 
 dnj.hooks = {
-    ['default'] = "", -- toto je fallback , ked neexistuje dnj.hooks pre dany channel , pouzije sa tento webhook
+    ['default'] = "", -- default je fallback , ked neexistuje dnj.hooks pre dany channel , pouzije sa tento webhook
 }
 
 dnj.colors = {
-    ['info'] = 3447003, -- info je tiez fallback , ked typ farby neexistuje automaticky to ma farbu infa (modru)
+    ['info'] = 3447003, -- info je tiez fallback , ked typ farby neexistuje automaticky to ma farbu = info (modru)
     ['success'] = 5763719, 
     ['error'] = 15158332,  
     ['warning'] = 16776960 
