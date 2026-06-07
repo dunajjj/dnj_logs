8593ea1262d3b138e507944168c8c811d387bfa5
dnj <157141256+dunajjj@users.noreply.github.com>
2026-06-07 16:52:38 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 author 'dnj'
-shared_script {'shared/*.lua'}
-server_script {'server/*.lua'}
+--.shared_script {'shared/*.lua'}
+server_script {
+  'server/*.lua',
+  'shared/*.lua'
+}
 
 export 'sendlog'
